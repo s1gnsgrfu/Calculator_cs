150d760c9463378d7a00be4a76d208cfe9476d95
s1gnsgrfu <s.1gn.sgrfu@gmail.com>
2022-09-19 17:40:17 +0900

    Supports decimal point calculations

--- Calculator.cs
+++ Calculator.cs
@@ -18,25 +18,25 @@ namespace Calculator
 {
     class calm
     {
-        public static int? count = 0, va1 = 0, va2 = 0, culflg = 0, result = 0, flag = 0;
+        private static int? count = 0, culflg = 0, flag = 0, culflg2 = 0;
+        private static int prod1 = 0, prod2 = 0, c_cnt1 = 0, c_pcnt1 = 0, c_pcnt2 = 0, c_cnt2 = 0, carr1 = 0, carr2 = 0, tmpcf = 0, err = 0, cntp1 = 0, cntp2 = 0;
+        private static decimal va1 = 0, va2 = 0, result = 0, ctmp1_1, ctmp1_2 = 0, ctmp2_1 = 0, ctmp2_2 = 0, pow = 1;
+        private static string tmp;
         //culflg -> 1:+ , 2:- , 3:* , 4:/
         //flag -> null:exit , 2:result
-        //private static Dictionary<キーの型/クラス, > f = new Dictionary<キーの型/クラス, 値の型/クラス>();
         static int Main()
         {
-            //List <string> ma = new List <string>();
-            //int? result, flag;
-            string str1, str2, str3;
-            //int[] flag = new int[2];
             calm c1 = new calm();
 
             Console.WriteLine("Calculator");
             Console.WriteLine("If you need help , type --help");
+
             while (true)
             {
-
+                Console.WriteLine("");
                 Console.Write(">>");
                 flag = c1.Stsp(Console.ReadLine());
+
                 if (flag == null)
                 {
                     return 0;
@@ -45,24 +45,106 @@ namespace Calculator
                     Console.WriteLine("----------");
                     Console.WriteLine(result);
                     Console.WriteLine("----------");
-                    count = va1 = va2 = culflg = result = flag = 0;
+                    count = culflg = flag = culflg2 = 0;
+                    prod1 = prod2 = c_cnt1 = c_pcnt1 = c_pcnt2 = c_cnt2 = carr1 = carr2 = tmpcf = 0;
+                    va1 = va2 = result = ctmp1_1 = ctmp1_2 = ctmp2_1 = ctmp2_2 = 0;
+                    //処理終了初期化
                 }
                 else
                 {
                     ;
                 }
-                //c1.Stsp(str1);
             }
         }
 
-        private int? Stsp(string str)
+        private int? Stsp(in string str)
         {
             calm c1 = new calm();
-            //int count, va1, va2;
-            Console.WriteLine("1 -- typed --> " + str);
+
+            count++;
+            culflg2 = prod1 = prod2 = cntp1 = cntp2 = 0;
+            pow = 1;
+
+            if ((va1 == 0) && (err == 1)) 
+            {
+                c_cnt1 = c_pcnt1 = carr1 = 0;
+            }
+            else if ((va1 != 0) && (err == 1)) 
+            {
+                c_cnt2 = c_pcnt2 = carr2 = 0;
+            }
+
+            err = 0;
+
+            foreach (char tmp in str)
+            {
+                if (va1 == 0)
+                {
+                    c_cnt1++;               //c_cntに-1すればピリオド前の数値数
+                                            //c_cntからc_pcntを引けばピリオド後の数値数
+                    if (tmp == '.')
+                    {
+                        if (prod1 == 0)
+                        {
+                            prod1 = 1;
+                            cntp1++;
+                            c_pcnt1 = c_cnt1;
+                        }
+                        else
+                        {
+                            cntp1++;
+                        }
+                        
+                    }
+                }
+                else if ((c_cnt1 != 1) && (tmpcf == 1)) 
+                {
+                    c_cnt2++;
+
+                    if (tmp == '.')
+                    {
+                        if (prod2 == 0)
+                        {
+                            prod2 = 1;
+                            cntp2++;
+                            c_pcnt2 = c_cnt2;
+                        }
+                        else
+                        {
+                            cntp2++;
+                        }
+                    }
+                }
+
+                switch (tmp)
+                {
+                    case '+':
+                        culflg2 = 1;
+                        break;
+                    case '-':
+                        culflg2 = 2;
+                        break;
+                    case '*':
+                        culflg2 = 3;
+                        break;
+                    case '/':
+                        culflg2 = 4;
+                        break;
+                }
+                //Console.WriteLine(culflg);
+            }
+            /*
+            Console.WriteLine("c_cnt1  --> "+c_cnt1);
+            Console.WriteLine("c_pcnt1 -->"+c_pcnt1);
+            Console.WriteLine("c_cnt2  --> " + c_cnt2);
+            Console.WriteLine("c_pcnt2  -->" + c_pcnt2);
+            Console.WriteLine("prod1 --> " + prod1);
+            Console.WriteLine("pro21 --> " + prod2);
+            */
             if (Regex.IsMatch(str, "^--"))
             {
-                Console.WriteLine(true);
+                //Console.WriteLine(true);
+
                 switch (str[2..^0])
                 {
                     case "help":
@@ -76,46 +158,49 @@ namespace Calculator
                         Console.WriteLine("This command is not defined");
                         return 0;
                 }
+            } else if ((prod1 == 1) && (culflg2 == 0) && (cntp1 == 1)) 
+            {
+                carr1 = c_cnt1 - c_pcnt1;                       //小数点後の数値の数
+                tmp = str.Substring(c_pcnt1, carr1);            //小数点後の数値（文字列）
+                ctmp1_1 = decimal.Parse(str[0..^carr1]);        //小数点前の数値をdecimal型に変換
+                ctmp1_2 = decimal.Parse(tmp);                   //小数点後の数値をdecimal型に変換
+
+                for (int i = 0; i < carr1; i++)
+                {
+                    pow = Decimal.Multiply(pow, 0.1m);
+                }
+                va1 = ctmp1_1 + ctmp1_2 * pow;                  //小数点前後の数値を結合
+                return 0;
+            } else if ((prod2 == 1) && (culflg2 == 0) && (va2 == 0) && (cntp2 == 1))   
+            {
+                carr2 = c_cnt2 - c_pcnt2;                       //小数点後の数値の数
+                tmp = str.Substring(c_pcnt2, carr2);            //小数点後の数値（文字列）
+                ctmp2_1 = decimal.Parse(str[0..^carr2]);        //小数点前の数値をdecimal型に変換
+                ctmp2_2 = decimal.Parse(tmp);                   //小数点後の数値をdecimal型に変換
+
+                for (int i = 0; i < carr2; i++)
+                {
+                    pow = Decimal.Multiply(pow, 0.1m);
+                }
+                va2 = ctmp2_1 + ctmp2_2 * pow;                  //小数点前後の数値を結合
+                return 0;
             }
-            else if (Regex.IsMatch(str, "^[0-9]+$"))  //すべて数字（1回目）
+            else if (Regex.IsMatch(str, "^([0-9])+$"))          //すべて数字
             {
-                if (count == 0)
+                if ((culflg2 == 0) && (va1 == 0))
                 {
-                    count++;
-                    Console.WriteLine("int-" + count);
-                    va1 = int.Parse(str);
-                    Console.WriteLine("va1 -> " + va1);
-                    //return 0;
+                    va1 = decimal.Parse(str);
                 }
-                else if ((count == 1)&&(culflg != 0))
+                else if ((culflg2 == 0) && (va2 == 0) && (culflg != 0)) 
                 {
-                    count++;
-                    Console.WriteLine("int-" + count);
-                    va2 = int.Parse(str);
-                    Console.WriteLine("va2 -> " + va2);
-                    //return 0;
+                    va2 = decimal.Parse(str);
                 }
                 else
                 {
-                    Console.WriteLine("ennzannsi kure");
-                    
+                    Console.WriteLine("please enter the operator");
                 }
                 return 0;
-            }/*
-            else if (str == "=")
-            {
-                if (count > 0)
-                {
-                    Console.WriteLine("eq");
-                    //return 0;
-                }
-                else
-                {
-                    Console.WriteLine("Error");
-                    //return 0;
-                }
-                return 0;
-            }*/
+            }
             else if (str == "exit")
             {
                 return null;
@@ -125,133 +210,55 @@ namespace Calculator
                 switch (str)
                 {
                     case "+":
-                        Console.WriteLine("sw-+");
                         culflg = 1;
+                        tmpcf = 1;
+                        prod1 = 0;
                         Console.WriteLine(culflg);
                         break;
                     case "-":
-                        Console.WriteLine("sw--");
                         culflg = 2;
+                        tmpcf = 1;
+                        prod1 = 0;
                         break;
                     case "*":
-                        Console.WriteLine("sw-*");
                         culflg = 3;
+                        tmpcf = 1;
+                        prod1 = 0;
                         break;
                     case "/":
-                        Console.WriteLine("sw-/");
                         culflg = 4;
+                        tmpcf = 1;
+                        prod1 = 0;
                         break;
                     case "=":
-                        Console.WriteLine("sw-=");
-                        //culflg = 5; //計算専用関数に飛べばいいかも
                         Eq();
                         return 2;
-                        //break;
                     default:
-                        if (count == 1)
+                        if ((va1 != 0) && (culflg == 0)) 
                         {
-                            Console.WriteLine("ennzannsi kure");
+                            Console.WriteLine("please enter the operator");
                         }
                         else
                         {
                             Console.WriteLine("Error");
+                            err = 1;
                         }
                         break;
                 }
-                //Console.WriteLine("else");
                 return 0;
             }
-            /*
-            //sf -> 演算、被演算数値フラグ
-            //i -> 文字数
-            //cf -> 1:+ , 2:- , 3:* , 4:/ , 5:=
-            int sf = 0, i = 0, ic1 = 0, ic2 = 0, cf = 0, ex = 0;
-            double res;
-            double[] d = new double[2];
-            List<int> cd1 = new List<int>();
-            List<int> cd2 = new List<int>();
-
-            foreach (char c in ma)
-            {
-                //Console.WriteLine(c);
-                
-
-                //string isNumeric = int.TryParse(c, out n);
-                if (char.IsNumber(c))
-                {
-                    if (sf == 0)
-                    {
-                        ic1++;  //1--
-                        cd1.Add(c);
-                    }else if(sf == 1)
-                    {
-                        ic2++;  //1--
-                        cd2.Add(c);
-                    }
-                    Console.Write(c + " --> ");
-                    Console.WriteLine("digit");
-                    Console.WriteLine(ma);
-
-                }
-                else
-                {
-                    if(sf == 0)
-                    {
-                        switch (c)
-                        {
-                            case '+':
-                                cf = 1;
-                                break;
-                            case '-':
-                                cf = 2;
-                                break;
-                            case '*':
-                                cf = 3;
-                                break;
-                            case '/':
-                                cf = 4;
-                                break;
-                            case '=':
-                                cf = 5;
-                                break;
-                            default:
-                                Console.WriteLine("Error");
-                                ex = 1;
-                                break;
-                        }
-                    }else if ((sf == 1) || (sf == 9))   //2回目および=
-                    {
-                        d = Digi(cd1, cd2, ic1, ic2);
-                        res = Caltion();
-                    }
-                    
-                    Console.Write(c + " --> ");
-                    Console.WriteLine("No digit");
-                    Console.WriteLine(ma);
-                    sf = 1;
-                }
-
-                i++;    //0--
-            */
-            //}
         }
         
         private int? Eq()
         {
             calm c1 = new calm();
-            Console.WriteLine("clu->" + culflg);
-            
-            //Console.WriteLine("----------");
-            
-            //Console.Write(va1+" ");
+
             switch (culflg)
             {
                 case 1:
-                    //Console.WriteLine("++++++++++++" + va1 + "==" + va2);
                     result = va1 + va2;
                     break;
                 case 2:
-                    Console.WriteLine("--------" + va1 + "==" + va2);
                     result = va1 - va2;
                     break;
                 case 3:
@@ -261,31 +268,7 @@ namespace Calculator
                     result = va1 / va2;
                     break;
             }
-            //Console.Write(va2 + " ");
-
-
-            //Console.WriteLine()
-            
             return 0;
         }
-    /*
-        //private double List<int> cd1 = new List<int>();
-        private double[] Digi(IReadOnlyCollection<int> cd1,IReadOnlyCollection<int> cd2,int ic1,int ic2)
-        {
-            foreach(double c1 in cd1)
-            {
-                if (ic1 == 1)
-                {
-                    double res1 = c1;
-                }
-            }
-        }
-        private void Caltion()
-        {
-
-        }*/
-
-
     }
-
 }
