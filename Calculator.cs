b5dcf742ed898f9af2caab674689ffd391cbc288
s1gnsgrfu <s.1gn.sgrfu@gmail.com>
2022-09-23 14:08:13 +0900

    Change input format

--- Calculator.cs
+++ Calculator.cs
@@ -11,13 +11,15 @@ using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using System.IO;
 using System.Text;
+using System.Runtime.ExceptionServices;
+using System.Diagnostics;
 
 namespace Calculator
 {
     class calm
     {
         private static int? culflg = 0, flag = 0, culflg2 = 0;
-        private static int prod1 = 0, prod2 = 0, tmpcf = 0, res = 0, pi_flag = 0;
+        private static int prod1 = 0, prod2 = 0, tmpcf = 0, res = 0, pi_flag = 0, val_flag = 0, count = 0, re = 0;
         private static decimal result = 0, pow = 1;
         //private static string? tmp;
         private int[] c_cnt = new int[2];
@@ -25,7 +27,7 @@ namespace Calculator
         private int[] carr = new int[2];
         private int[] cntp = new int[2];
         private int[] err = new int[2];
-        private decimal[] va = new decimal[2];
+        private decimal[] va = new decimal[3];
         private decimal[] ctmp = new decimal[4];
         string[] split_str = new string[99];        //100項まで
 
@@ -40,7 +42,7 @@ namespace Calculator
             if (reset_type == 0)        //全変数初期化
             {
                 culflg = flag = culflg2 = 0;
-                prod1 = prod2 = tmpcf = res = pi_flag = 0;
+                prod1 = prod2 = tmpcf = res = pi_flag = val_flag = count = 0;
                 result = 0;
                 pow = 1;
                 for (int i = 0; i < 2; i++)
@@ -63,7 +65,7 @@ namespace Calculator
             }
             else if (reset_type == 1)       //第3項以降の変数初期化
             {
-                va[0] = result;
+                //va[0] = result;
                 tmpcf = c_cnt[0] = 1;
                 flag = 0;
                 carr[1] = c_pcnt[1] = c_cnt[1] = 0;
@@ -71,15 +73,22 @@ namespace Calculator
             }
             return;
         }
-        static int Main()
+
+        static void Main()
+        {
+            Console.WriteLine("Calculator");
+            Console.WriteLine("If you need help , type --help");
+            InOutput();
+        }
+        static int InOutput()
         {
             calm c1 = new calm();
 
-            Console.WriteLine("Calculator");
-            Console.WriteLine("If you need help , type --help");
+
 
             while (true)
             {
+                c1.Variable_initialization(0);
                 Console.WriteLine("");
                 Console.Write(">>");
                 flag = c1.Identification_string(Console.ReadLine());
@@ -100,7 +109,9 @@ namespace Calculator
                     }
                     else if (res == 1)      //第3項以降 --> 連続処理に要変更
                     {
-                        c1.Variable_initialization(1);
+                        c1.Continuous_process();
+                        
+                        
                     }
                 }else if(flag == 3)     //100項を超えたとき または 全変数初期化
                 {
@@ -113,9 +124,20 @@ namespace Calculator
             }
         }
 
+        private void Continuous_process()
+        {
+            //Console.WriteLine("result --> " + result);    //debug
+            va[0] = result;
+            va[1] = 0;
+            count = 1;
+            Variable_initialization(1);     //?
+            return;
+        }
+
         private int? Identification_string(in string str)
         {
             calm c1 = new calm();
+            //Console.WriteLine("str --> " + str);
             if (Regex.IsMatch(str, "^--"))
             {
                 Disp_option(str);
@@ -131,7 +153,7 @@ namespace Calculator
                 return 3;
             }
 
-
+            /*
             //エラー時のロールバック
             if ((va[0] == 0) && (err[0] == 1))
             {
@@ -141,16 +163,19 @@ namespace Calculator
             {
                 c_cnt[1] = c_pcnt[1] = carr[1] = 0;
             }
+            */
 
             culflg2 = prod1 = prod2 = cntp[0] = cntp[1] = 0;
             pow = 1;
             err[0] = 0;
 
+            /*
             foreach (char tmpc in str)
             {
                 String_analyze(tmpc);
 
             }
+            */
 
             //debug
             /*
@@ -164,7 +189,7 @@ namespace Calculator
             Console.WriteLine("culflg2 --> " + culflg2);
             */
 
-            
+            /*
             if (str == ".")
             {
                 Console.WriteLine("Error");
@@ -201,6 +226,41 @@ namespace Calculator
 
             }
             //return 0;
+            */
+            
+            for (int i = 0; i < split_str.Length; i++) {
+                //if ((Regex.IsMatch(split_str[i],"^([0-9])+$")))
+                String_analyze(split_str[i]);
+                //count++;
+                if (re == 4)
+                {
+                    res = 0;
+                    return 2;
+                }else if(re == 6){
+                    break;
+                }
+            }
+            return 3;
+            /*
+            if ((Regex.IsMatch(split_str[0], "^([0-9])+$")) == false)       //初項数字以外
+            {
+                foreach(char c in split_str[0])
+                {
+                    String_analyze(c);
+                }
+                //err[1] = 0;
+                return 3;
+            }
+            for (int i = 0; i < str.Length; i++)
+            {
+                
+                if (Regex.IsMatch(split_str[i], "^([0-9])+$"))
+                {
+                    val_flag = 1;
+
+                }
+            }
+            */
         }
         
         private void Split_string(string str)
@@ -246,7 +306,7 @@ namespace Calculator
                     }
                     catch (System.IndexOutOfRangeException)
                     {
-                        Console.WriteLine("Please enter an expression of no more than 100 terms.");
+                        Console.WriteLine("Both numbers and operators must be limited to 100.");
                         err[1] = 1;
                         return;
                     }
@@ -259,17 +319,85 @@ namespace Calculator
             {
                 count_blank--;
             }
-            
+            /*
             for(int i = 0; i <= count_blank; i++)       //debug
             {
                 Console.WriteLine("split_str[" + i + "] --> " + split_str[i]);
             }
-            
+            */
 
             return;
         }
-        private void String_analyze(char tmpc)
+        private void String_analyze(string str)
         {
+            //Console.WriteLine("va[" + count + "] --> " + va[count]);
+            //Console.WriteLine("str --> " + str);
+            re = 0;
+            //if((Regex.IsMatch(str,"^([0-9])+$")))
+            try
+            {
+                if (decimal.TryParse(str, out va[count]))
+                {
+                    /*
+                    Console.WriteLine("str --> " + str);        //debug
+                    Console.WriteLine("TryParse - count --> " + count);
+                    Console.WriteLine("va[" + count + "] --> " + va[count]);
+                    */
+
+                    count++;
+                }
+                else if (str == "pi")
+                {
+                    va[count] = pi;
+                    count++;
+                }
+                else
+                {
+                    switch (str)
+                    {
+                        case "+":
+                        case "-":
+                        case "*":
+                        case "/":
+                        case "=":
+                            re = Identification_operator(str);
+                            if (re == 2)
+                            {
+                                Continuous_process();
+                                re = 0;
+                            }
+                            else if (re == 4)
+                            {
+                                return;
+                            }
+                            break;
+                    }
+                }
+            }
+            catch(System.IndexOutOfRangeException)
+            {
+                Console.WriteLine("Error");
+                //Variable_initialization(0);
+                re = 6;
+            }
+            /*
+            if ((char.IsNumber(tmpc))&&(first==0))
+            {
+                first = 1;
+
+            }
+            switch (tmpc)
+            {
+                case '.':
+                    prod1++;
+                    break;
+                default:
+                    tmpcf = 0;
+                    Identification_operator(tmpc);
+                    break;
+            }
+            */
+            /*
             if (va[0] == 0)
             {
                 c_cnt[0]++;               //c_cntに-1すればピリオド前の数値数
@@ -324,6 +452,7 @@ namespace Calculator
                     culflg2 = 4;
                     break;
             }
+            */
         }
 
         private void Disp_option(string str)
@@ -431,7 +560,6 @@ namespace Calculator
             switch (str)
             {
                 case "+":
-                case "q":
                     if (tmpcf == 1)
                     {
                         Calculation();
@@ -443,7 +571,6 @@ namespace Calculator
                     tmpcf = 1;
                     break;
                 case "-":
-                case "w":
                     if (tmpcf == 1)
                     {
                         Calculation();
@@ -455,7 +582,6 @@ namespace Calculator
                     tmpcf = 1;
                     break;
                 case "*":
-                case "e":
                     if (tmpcf == 1)
                     {
                         Calculation();
@@ -467,7 +593,6 @@ namespace Calculator
                     tmpcf = 1;
                     break;
                 case "/":
-                case "r":
                     if (tmpcf == 1)
                     {
                         Calculation();
@@ -480,20 +605,23 @@ namespace Calculator
                     tmpcf = 1;
                     break;
                 case "=":
-                case "a":
                     //Console.WriteLine("culflg --> " + culflg);    //debug
                     Calculation();
-                    return 2;
+                    return 4;
                 default:
+                    /*
                     if ((va[0] != 0) && (culflg == 0))
                     {
                         Console.WriteLine("please enter the operator");
                     }
                     else
                     {
+                    */
                         Console.WriteLine("Error");
                         err[0] = 1;
-                    }
+                        c1.Variable_initialization(0);
+                        //InOutput();
+                    //}
                     break;
             }
             return 0;
@@ -523,6 +651,8 @@ namespace Calculator
                     {
                         Console.WriteLine("Error");
                         res = 1;
+                        c1.Variable_initialization(0);
+                        //InOutput();
                     }
                     break;
             }
